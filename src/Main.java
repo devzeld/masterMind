81c613693760c50f87df3cf8dacd132a66576fea
devzeld <zeld.exe.dev@gmail.com>
2023-10-13 18:23:43 +0200

    Finished

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 
 public class Main extends MasterMind{
     public static void main(String[] args) {
-        System.out.println(numInWrongPos(5930, 5094));
+        game();
     }
 }
--- src/MasterMind.java
+++ src/MasterMind.java
@@ -7,8 +7,9 @@ public class MasterMind{
         return 1000 + ran.nextInt(9000);
     }
 
-    public static int getUserNum(){
+    public static int getUserNum(int i){
         Scanner sc = new Scanner(System.in);
+        System.out.print(i + "- Insert a digit: ");
         return sc.nextInt();
     }
 
@@ -38,6 +39,8 @@ public class MasterMind{
     public static int numInWrongPos(int randomNum, int userNum){
         int counterWrongPos = 0;
         int numUserToTry;
+        int randN = randomNum;
+        int userN = userNum;
         for(int i = 1; i <= 4; i++) {
             numUserToTry = userNum % 10;
             if (verifyNum(numUserToTry, randomNum)) {
@@ -45,8 +48,23 @@ public class MasterMind{
             }
             userNum /= 10;
         }
-        return counterWrongPos;
+        return counterWrongPos - numInRightPos(randN, userN);
     }
 
+    public static void game(){
+        int i = 1;
+        int randomNum = getRandomNum();
+        while (i <= 10){
+            int userNum = getUserNum(i);
+            System.out.println(numInRightPos(randomNum, userNum) == 4?"You Won!" : "The number that you guess the position are: " + numInRightPos(randomNum, userNum) + "\nThe number that you guess are: " + numInWrongPos(randomNum, userNum));
+            if (numInRightPos(randomNum, userNum) == 4) {
+                break;
+            } else {
+                i++;
+            }
+
+        }
+        System.out.println(i > 10?"You probably lose!\nThe number was " + randomNum:"Good, the number was " + randomNum);
+    }
 
     }
